4d0e105ef6e9305e25a53f0e6efb874fbeecebb6
m7medayman <m7medmahgoop@gmail.com>
2025-05-03 02:06:30 +0300

    adjusting some styles in product card

--- src/features/home/model/home_model.js
+++ src/features/home/model/home_model.js
@@ -19,7 +19,7 @@ export class HomeModel {
     getProducts() {
         const products = [
             {
-                imageUrl: 'https://via.placeholder.com/300x200?text=Loveseat+Sofa',
+                imageUrl: './assets/images/test_product_img.png',
                 altText: 'Grey Loveseat Sofa',
                 isNew: true,
                 discountPct: 50,
@@ -29,6 +29,50 @@ export class HomeModel {
                 originalPrice: '$400.00',
                 rating: 5
             },
+            {
+                imageUrl: './assets/images/test_product_img.png',
+                altText: 'Grey Loveseat Sofa',
+                isNew: true,
+                isFavorite: false,
+                title: 'Loveseat Sofa',
+                price: '$199.00',
+
+                rating: 3
+            },
+            {
+                imageUrl: './assets/images/test_product_img.png',
+                altText: 'Grey Loveseat Sofa',
+                isNew: true,
+                discountPct: 50,
+                isFavorite: false,
+                title: 'Loveseat Sofa',
+                price: '$199.00',
+                originalPrice: '$400.00',
+                rating: 0
+            },
+            {
+                imageUrl: './assets/images/test_product_img.png',
+                altText: 'Grey Loveseat Sofa',
+                isNew: true,
+                discountPct: 50,
+                isFavorite: false,
+                title: 'Loveseat Sofa',
+                price: '$199.00',
+                originalPrice: '$400.00',
+                rating: 1
+            },
+            {
+                imageUrl: './assets/images/test_product_img.png',
+                altText: 'Grey Loveseat Sofa',
+                isNew: true,
+                discountPct: 20,
+                isFavorite: false,
+                title: 'Loveseat Sofa',
+                price: '$199.00',
+                originalPrice: '$400.00',
+                rating: 2
+            },
+
             {
                 imageUrl: 'https://via.placeholder.com/300x200?text=Loveseat+Sofa',
                 altText: 'Grey Loveseat Sofa',
