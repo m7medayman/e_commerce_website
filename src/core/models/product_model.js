185cea50951784dbd385f7211c1a548c14a74ccd
m7medayman <m7medmahgoop@gmail.com>
2025-05-03 17:35:25 +0300

    fixing models

--- src/core/models/product_model.js
+++ src/core/models/product_model.js
@@ -45,4 +45,10 @@ export class ProductModel {
       localStorage.setItem(this.STORAGE_KEY, JSON.stringify(products));
     }
   }
-  
+  function generateUUID() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        const r = Math.random() * 16 | 0;
+        const v = c === 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
--- src/core/models/seller_model.js
+++ src/core/models/seller_model.js
@@ -1,3 +1,4 @@
+
 export class SellerAnalyticsModel {
     static STORAGE_KEY = 'sellerAnalytics';
 
