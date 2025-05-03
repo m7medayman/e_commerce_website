7b734a38c8b0801f5c1a56485c595949c3d302f9
Mohamed Magoor <mohamedmagoor6@gmail.com>
2025-05-03 16:52:29 +0300

    make product details mvc ui

--- src/features/product/controller/product_controller.js
+++ src/features/product/controller/product_controller.js
@@ -1,16 +1,35 @@
-// import { ProductModel } from "../model/product_model.js";
-// import { ProductView } from "../view/product_view.js";
+
+
+import {ProductModel} from "../../../core/models/product_model.js";   
 
 export class ProductController {
-  constructor(model, view) {
-    this.model = model;
+  constructor(view) {
     this.view = view;
   }
 
   init() {
     const params = new URLSearchParams(window.location.search);
     const id = params.get("id");
-    const product = this.model.getProductById(id);
-    this.view.render(product);
+
+    const product = ProductModel.getById(id); 
+
+    if (product) {
+      this.view.render(product);
+    } else {
+      this.view.renderError("Product not fلهفound");
+    }
+    // // Dummy Data بدلاً من استرجاع المنتج من الـ localStorage
+    // const dummyProduct = {
+    //   id: 1,
+    //   name: "Sample Product",
+    //   img: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/Lion_d%27Afrique.jpg/1099px-Lion_d%27Afrique.jpg",
+    //   description: "This is a description of the sample product.",
+    //   price: "$99.99",
+    //   discount: "$79.99",
+    //   measurements: "10 x 5 x 2 cm",
+    // };
+
+    // // مباشرة تمرير الـ dummyProduct للـ view
+    // this.view.render(dummyProduct);
   }
 }
--- src/features/product/model/product_model.js
+++ src/features/product/model/product_model.js
@@ -1,9 +1,12 @@
-export class ProductModel {
-    constructor() {
-      this.products = JSON.parse(localStorage.getItem("products")) || [];
-    }
+// export class ProductModel {
+//     constructor() {
+//       this.products = JSON.parse(localStorage.getItem("products")) || [];
+//     }
   
-    getProductById(id) {
-      return this.products.find(product => product.id === id);
-    }
-  }
+//     getProductById(id) {
+//       return this.products.find(product => product.id === id);
+//     }
+//   }
+// import { ProductModel } from "../../../core/models/product_model.js";
+
+// const product = ProductModel.getById(id);
