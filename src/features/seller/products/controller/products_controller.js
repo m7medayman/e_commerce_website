01851b28cd948e3b5ef5b2a1c8d0293afee96095
Hanan-Ashraf-Mousa <mosah2312@gmail.com>
2025-05-05 23:53:57 +0300

    create files of products seller with mvc

---
+++ src/features/seller/products/controller/products_controller.js
@@ -0,0 +1,7 @@
+// import { ProductsView } from "../view/products_view";
+// export class ProductsController{
+//     constructor(){
+//         this.view= new ProductsView();
+//     }
+   
+// }
---
+++ src/features/seller/products/view/products_view.js
@@ -0,0 +1,10 @@
+// export class ProductsView{
+//     constructor(){
+//        this.products= document.getElementById('container');
+//        this.render();
+//     }
+//     render(){
+//         this.products.innerHTML=`<div class="col-12 text-end" id="add_button"> <button class="btn btn-dark" id="addProduct">Add New Product</button></div>
+//             <div id="products">`;
+//     }
+// }
