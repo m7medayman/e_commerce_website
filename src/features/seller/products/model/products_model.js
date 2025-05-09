81ccbdf36e08bd827a998ad900b1f199d2512f7a
m7medayman <m7medmahgoop@gmail.com>
2025-05-09 23:28:27 +0300

    Update sellerId assignment in ProductsModel to retrieve from local storage

--- src/features/seller/products/model/products_model.js
+++ src/features/seller/products/model/products_model.js
@@ -3,11 +3,8 @@ import { ProductModel } from "../../../../core/models/product_model.js";
 export class ProductsModel {
     constructor() {
         this.allProducts = ProductModel.getAll();
-<<<<<<< HEAD
-        this.sellerId = 'seller-1';
-=======
-        this.sellerId=localStorage.getItem(AuthModel.STORAGE_KEY);
->>>>>>> seller
+
+        this.sellerId = localStorage.getItem(AuthModel.STORAGE_KEY);
     }
     getBySellerId() {
         return this.allProducts.filter(product => product.sellerId === this.sellerId);
