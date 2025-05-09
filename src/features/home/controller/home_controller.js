3fcda9e3f89b2c7a485e74e4d45e005befd2345d
m7medayman <m7medmahgoop@gmail.com>
2025-05-09 22:00:23 +0300

    feat: integrate authentication checks for adding products to cart and wishlist

--- src/features/home/controller/home_controller.js
+++ src/features/home/controller/home_controller.js
@@ -4,6 +4,10 @@ import { DummyData } from '../../../core/models/dummy_data.js';
 import { ProductModel } from '../../../core/models/product_model.js';
 import { CartModel } from '../../../core/models/cart_model.js';
 import { WishlistModel } from '../../../core/models/wish_model.js'
+import { AuthModel } from "../../../core/models/auth_model.js"
+import { AuthPopupController } from "../../../core/common/auth_checker/auth_checker_controller.js"
+
+
 export class HomeController {
     constructor() {
         this.model = new HomeModel();
@@ -12,24 +16,28 @@ export class HomeController {
     }
 
     init() {
+        this.auth = AuthModel.getUser();
+        this.isLoggedIn = !(this.auth == null);
+        console.log(this.isLoggedIn);
         // DummyData.clearTheLocalStorage(); // to clear the old data if there is any 
         DummyData.generateDummyProductData(); // that will generate dummy product data and add it to the cart 
         let images = this.model.getImages();
         let products = this.model.getProducts();
         // Get the favorite list once before mapping products
-        let favoriteList = (WishlistModel.getByUserId("user-1")["items"]).map(item => item.productId);
 
-        products = products.map(function (product) {
-            let f = favoriteList.includes(product.id);
-            product.isFavorite = favoriteList.includes(product.id);
-            return product;
-        });
+        if (this.isLoggedIn) {
+            let favoriteList = (WishlistModel.getByUserId(this.auth.userId)["items"]).map(item => item.productId);
+            products = products.map(product => {
+                product.isFavorite = favoriteList.includes(product.id);
+                return product;
+            });
+        }
         console.log(products);
         let threeImageSection = this.model.getThreeImageSection();
         this.view.renderPage();
         this.view.renderCarousel(images);
         this.view.renderNewProducts(products);
-        this.view.addEventListenerToProductCard(this.addAddToCartEventListener, this.toggelProductInWishList, this.goToProductPage);
+        this.view.addEventListenerToProductCard(this.addAddToCartEventListener.bind(this), this.toggelProductInWishList.bind(this), this.goToProductPage);
         // this.view.addAddToCartEventListener(this.addAddToCartEventListener);
         // this.view.addWishlistEventListener();
         this.view.renderThreeImageSection(threeImageSection[0], threeImageSection[1], threeImageSection[2]);
@@ -39,23 +47,32 @@ export class HomeController {
 
     }
     addAddToCartEventListener(id) {
-        const userId = "user-1";
+        if (!this.isLoggedIn) {
+            AuthPopupController.show("to add product");
+            return;
+        }
+
+        const userId = this.auth.userId;
         CartModel.addItem(userId, id, 1);
         console.log("Add to cart clicked for product ID:", id);
 
     }
     toggelProductInWishList(produtId, isFavorite) {
+        if (!this.isLoggedIn) {
+            AuthPopupController.show("to add product");
+            return;
+        }
+
+        const userId = this.auth.userId;
         if (isFavorite) {
-            WishlistModel.addItem("user-1", ProductModel.getById(produtId));
+            WishlistModel.addItem(userId, ProductModel.getById(produtId));
         }
         else {
-            WishlistModel.removeItem("user-1", produtId);
+            WishlistModel.removeItem(userId, produtId);
         }
     }
     goToProductPage(id) {
         window.location.href = `./product_details.html?id=${id}`;
     }
-    setupEventListeners() {
-        // Add any event listeners here if needed
-    }
+
 }
--- src/features/shop/controller/shop_controller.js
+++ src/features/shop/controller/shop_controller.js
@@ -4,6 +4,7 @@ import { ProductModel } from '../../../core/models/product_model.js';
 import { CartModel } from '../../../core/models/cart_model.js';
 import { WishlistModel } from '../../../core/models/wish_model.js'
 import { AuthPopupController } from "../../../core/common/auth_checker/auth_checker_controller.js"
+import { AuthModel } from "../../../core/models/auth_model.js"
 export class ShopController {
     constructor() {
         this.model = new ShopModel();
@@ -15,6 +16,9 @@ export class ShopController {
     }
 
     init() {
+        this.auth = AuthModel.getUser();
+        this.isLoggedIn = !(this.auth == null);
+        console.log(this.isLoggedIn);
         // Get categories for filter dropdown
         const categories = this.model.getCategories();
 
@@ -27,7 +31,7 @@ export class ShopController {
         // Add filter event listeners
         this.view.addFilterEventListeners(this.handleFilter.bind(this));
 
-        // Add product card event listeners
+
 
     }
 
@@ -45,36 +49,49 @@ export class ShopController {
         let products = this.model.getProducts(this.filters);
 
         // Apply wishlist status
-        let favoriteList = (WishlistModel.getByUserId("user-1")["items"]).map(item => item.productId);
-        products = products.map(product => {
-            product.isFavorite = favoriteList.includes(product.id);
-            return product;
-        });
+        if (this.isLoggedIn) {
+            let favoriteList = (WishlistModel.getByUserId(this.auth.userId)["items"]).map(item => item.productId);
+            products = products.map(product => {
+                product.isFavorite = favoriteList.includes(product.id);
+                return product;
+            });
+        }
+
 
         // Render products
         this.view.renderNewProducts(products);
 
         // Re-add event listeners to new product cards
         this.view.addEventListenerToProductCard(
-            this.addAddToCartEventListener,
-            this.toggleProductInWishList,
-            this.goToProductPage
+            this.addAddToCartEventListener.bind(this),
+            this.toggleProductInWishList.bind(this),
+            this.goToProductPage.bind(this)
         );
 
     }
     addAddToCartEventListener(id) {
-        AuthPopupController.show("test action")
-        const userId = "user-1";
+        if (!this.isLoggedIn) {
+            AuthPopupController.show("to add product");
+            return;
+        }
+
+        const userId = this.auth.userId;
         CartModel.addItem(userId, id, 1);
         console.log("Add to cart clicked for product ID:", id);
 
     }
     toggleProductInWishList(produtId, isFavorite) {
+        if (!this.isLoggedIn) {
+            AuthPopupController.show("to add product");
+            return;
+        }
+
+        const userId = this.auth.userId;
         if (isFavorite) {
-            WishlistModel.addItem("user-1", ProductModel.getById(produtId));
+            WishlistModel.addItem(userId, ProductModel.getById(produtId));
         }
         else {
-            WishlistModel.removeItem("user-1", produtId);
+            WishlistModel.removeItem(userId, produtId);
         }
     }
     goToProductPage(id) {
